35e73e1144b4c77277dd933f0f2a7bfbce2672f0
Shailesh Patil <pshailesh1612@gmail.com>
2010-02-17 17:48:41 +0530

    changed array.count to size and read the database configurations from the yml

--- load.rb
+++ load.rb
@@ -1,14 +1,17 @@
+
 require 'config/environment.rb'
 require 'mysql'
 
 system('nohup ruby script/generate leave_migration &')
 sleep(5)
-system('nohup rake RAILS_ENV=production db:migrate &')
+system("nohup rake RAILS_ENV=#{RAILS_ENV} db:migrate &")
 
 mysql = Mysql.init()
 
 # change  the mysql connection parameters accordingly
-mysql.connect('localhost', 'ejosh', 'josh123@', 'eJosh')
+dbconfig = ActiveRecord::Base.configurations[RAILS_ENV] 
+
+mysql.connect(dbconfig['host'], dbconfig['username'], dbconfig['password'], dbconfig['database'])
 
 mysql.query("update extensions set entry_point = '/leave_management' where id = #{ARGV} ;")
 
